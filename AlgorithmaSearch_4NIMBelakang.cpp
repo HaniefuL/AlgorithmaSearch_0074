0466af615246d63e1cc94efded71cdd978e98ac0
HaniefuL <haniefulgeofarry@gmail.com>
2024-04-05 15:06:58 +0700

    menuliskan cin >> ch

--- AlgorithmaSearch_4NIMBelakang.cpp
+++ AlgorithmaSearch_4NIMBelakang.cpp
@@ -50,7 +50,14 @@ void LinearSearch()
 				cout << "\n" << item << " found at position " << (i + 1) << endl;
 				break;
 			}
-			i++;
+			i++; // step 4
 		}
+
+		if (i == n) // step 5
+			cout << "\n" << item << " not found in the array\n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
 	}
 }
