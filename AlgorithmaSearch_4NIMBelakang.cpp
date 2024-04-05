d0d9276e9654a58633ca198213abd2b92576499b
HaniefuL <haniefulgeofarry@gmail.com>
2024-04-05 14:49:35 +0700

    menuliskan void LinearSearch

--- AlgorithmaSearch_4NIMBelakang.cpp
+++ AlgorithmaSearch_4NIMBelakang.cpp
@@ -18,5 +18,14 @@ void input()
 	}
 
 	// Accept array elements
-
+	cout << "\n-----------------------\n";
+	cout << " Enter array elements \n";
+	cout << "----------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "< ";
+		cin >> arr[i];
+	}
 }
+
+void LinearSearch()
